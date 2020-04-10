0ca6165c945dd74a0bed3d7338a779a1d0779ba0
Chetan Verma <chetan61verma@gmail.com>
2020-04-10 19:28:34 +0530

    Added Login Page

---
+++ src/components/addContact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const AddContact = ()=>{
+    return(<div>
+        
+    </div>)
+}
+
+export default AddContact
---
+++ src/components/login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+
+const Login = ()=>{
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => console.log(data);
+    
+    return(
+    <div className="loginContainer">
+        <div className="logo">
+            <img src="/assets/logo.png"></img>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input name="email" type="email" ref={register({ required: true })} />
+            <input name="password" type="password" ref={register({ required:true })} />
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+    )
+}
+
+export default Login
